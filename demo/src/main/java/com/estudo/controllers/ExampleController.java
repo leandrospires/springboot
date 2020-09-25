05c435a551c3bc9f4c50071efd276e4f3d211dfb
LEANDRO SILVA PIRES <leandro.pires@portoseguro.com.br>
2020-09-25 14:16:15 -0300

    Primeira Aula

---
+++ demo/src/main/java/com/estudo/controllers/ExampleController.java
@@ -0,0 +1,18 @@
+package com.estudo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/example")
+public class ExampleController {
+	
+	@GetMapping("/hello")
+	public ResponseEntity<String> get() {
+		return ResponseEntity.ok("Hello => OK");
+	}
+	
+
+}
