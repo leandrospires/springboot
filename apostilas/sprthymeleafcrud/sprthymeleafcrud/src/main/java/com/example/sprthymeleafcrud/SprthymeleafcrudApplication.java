944e585060d3b85122dc0df758cca3101fc637d6
Leandro Pires <leandro.spires.br@gmail.com>
2020-09-29 15:20:06 -0300

    Teste ignore

---
+++ apostilas/sprthymeleafcrud/sprthymeleafcrud/src/main/java/com/example/sprthymeleafcrud/SprthymeleafcrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprthymeleafcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprthymeleafcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprthymeleafcrudApplication.class, args);
+	}
+
+}
