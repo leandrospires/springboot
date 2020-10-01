9acee9b017b8346471b9b45f45068b5519f35eb9
LEANDRO SILVA PIRES <leandro.pires@portoseguro.com.br>
2020-10-01 11:55:07 -0300

    Tentativa config web server

--- websocket/src/main/java/com/example/websocketdemo/WebsocketApplication.java
+++ websocket/src/main/java/com/example/websocketdemo/WebsocketApplication.java
@@ -2,10 +2,17 @@ package com.example.websocketdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebsocketApplication {
+public class WebsocketApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+		return builder.sources(WebsocketApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
 	}
