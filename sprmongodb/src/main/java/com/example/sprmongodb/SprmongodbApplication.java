ac0fe5ec348fa74229ab5472ad57676ae5f07d3d
Leandro Pires <leandro.spires.br@gmail.com>
2020-10-05 21:40:51 -0300

    App Final: Spring + Angular

---
+++ sprmongodb/src/main/java/com/example/sprmongodb/SprmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprmongodbApplication.class, args);
+	}
+
+}
---
+++ sprmongodb/src/test/java/com/example/sprmongodb/SprmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
